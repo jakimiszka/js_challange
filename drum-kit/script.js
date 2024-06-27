abcb87f45bbb771de8b36bab0ec965d88dbfcb14
jakimiszka <jarek.jakimicha@gmail.com>
2024-06-27 16:57:41 +0200

    some profress, added wave assets

--- drum-kit/script.js
+++ drum-kit/script.js
@@ -1,1 +1,14 @@
-console.log('vannila js challnage start');
+const buttons = document.getElementsByClassName('button');
+
+let beat = new Audio('./assets/boom.wav');
+
+const buttonsArray = [...buttons]
+    .map((button) => {
+        button.onclick = () => {
+            console.log(button.innerHTML + ' clicked!');
+            
+            // button press eeds some delay/loop action
+            // each button different sound
+            beat.play();
+        }
+});
